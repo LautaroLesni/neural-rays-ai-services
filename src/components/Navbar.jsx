e879100694b51526d26dacfa2dc1439940fb1e75
LautaroLesni <lautarolesniewicz@gmail.com>
2022-11-19 21:15:57 -0300

    feature/ stickynavbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import './Navbar.css'
 import customlogo from '../assets/neuralrays-ai.png'
+import { useState } from "react";
 
 const Navbar = () =>{
+    const [navbarscroll, setNavbarScroll] = useState(false)
+    const handleNavbarScroll = () => {
+        if (window.scrollY >= 100){
+            setNavbarScroll(true)
+        }
+        else{
+            setNavbarScroll(false)
+        }
+    }
+    window.addEventListener('scroll',handleNavbarScroll)
     return (
-        <div className="NavbarDIV">
+        <div className={navbarscroll ? 'NavbarDIV NavbarDIV-scroll' : 'NavbarDIV'}>
             <img src={customlogo} alt='neuralrays' className="customlogo"/>
             <ul className="ulnavbar">
                 <li>Home</li>
